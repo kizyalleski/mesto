736f1a7f29d2df217e289fcd09ca3cb2d7f0802b
Maxim Uspensky <kizyalleski@gmail.com>
2023-02-18 19:30:49 +0400

    feat: profile editing popup functionality is added

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,57 @@
 // КНОПКИ
 
-const editProfilePopup = document.querySelector("#edit-profile-popup"); // попап редактирования профиля
+const editProfilePopup = document.querySelector('#edit-profile-popup'); // попап редактирования профиля
 
 // функция открытия попапа
 function openPopup(popup) {
-  popup.classList.remove("popup__hidden");
+  popup.classList.remove('popup__hidden');
 }
 
 // функция закрытия попапа
 function closePoup(popup) {
-  popup.classList.add("popup__hidden");
+  popup.classList.add('popup__hidden');
 }
 
-// кнопка редактировать профиль
-const editProfileButton = document.querySelector("#edit-profile-button");
+// нажатие кнопки редактировать профиль
+const editProfileButton = document.querySelector('#edit-profile-button');
 if (!editProfileButton) {
-  throw new Error("NO editProfileButton");
+  throw new Error('NO editProfileButton');
 }
 
-editProfileButton.addEventListener("click", function () {
+editProfileButton.addEventListener('click', function () {
   openPopup(editProfilePopup);
 });
 
-// кнопка закрытыя редактирования профия
-const closePopupButton = document.querySelector("#close-popup-button");
+// нажатие кнопки закрытыя редактирования профия
+const closePopupButton = document.querySelector('#close-popup-button');
 if (!closePopupButton) {
-  throw new Error("NO closePopupButton");
+  throw new Error('NO closePopupButton');
 }
 
-closePopupButton.addEventListener("click", function () {
+closePopupButton.addEventListener('click', function () {
+  closePoup(editProfilePopup);
+});
+
+
+// нажатие кнопки сохранить
+const formSubmitButton = document.querySelector('#form-submit-button');
+if (!formSubmitButton) {
+  throw new Error('NO formSubmitButton')
+}
+
+formSubmitButton.addEventListener('click', function() {
+  event.preventDefault();
+  const formUserName = document.querySelector('#form-user-name').value;
+  if (!formUserName) {
+    throw new Error('NO formUserName')
+  }
+
+  const formUserOccupation = document.querySelector('#form-user-occupation').value;
+  if (!formUserOccupation) {
+    throw new Error('NO formUserOccupation')
+  }
+
+  document.querySelector('#profile-user-name').textContent = formUserName;
+  document.querySelector('#profile-user-occupation').textContent = formUserOccupation;
   closePoup(editProfilePopup);
 });
