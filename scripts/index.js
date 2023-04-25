2051ab2a3b2fed6f63bb9af66e4ac5ede558146d
Maxim Uspensky <kizyalleski@gmail.com>
2023-04-25 23:58:12 +0400

    fix: испавлены названия переменных и функций

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
-const editProfilePopup = document.querySelector("#editProfilePopup");
-const editProfileButton = document.querySelector("#editProfileButton");
-const closeEditProfilePopupButton = document.querySelector(
+const editingProfilePopup = document.querySelector("#editProfilePopup");
+const editingProfileButton = document.querySelector("#editProfileButton");
+const closingEditingProfilePopupButton = document.querySelector(
   "#closeEditProfilePopupButton"
 );
-const editProfileFormSubmitButton = document.querySelector(
+const editingProfileFormSubmitButton = document.querySelector(
   "#editProfileFormSubmitButton"
 );
 const profileUserName = document.querySelector("#profileUserName");
@@ -48,10 +48,9 @@ const cards = [
 ];
 
 // функция открытия попапа
-formUserName.value = profileUserName.textContent; // подстановка значений из основной страницы в попап
-formUserOccupation.value = profileUserOccupation.textContent;
-
 function openPopup(popup) {
+  formUserName.value = profileUserName.textContent; // подстановка значений из основной страницы в попап
+  formUserOccupation.value = profileUserOccupation.textContent;
   popup.classList.add("popup_opened");
 }
 
@@ -61,21 +60,21 @@ function closePopup(popup) {
 }
 
 // нажатие кнопки редактировать профиль
-editProfileButton.addEventListener("click", () => {
-  openPopup(editProfilePopup);
+editingProfileButton.addEventListener("click", () => {
+  openPopup(editingProfilePopup);
 });
 
 // нажатие кнопки закрытыя редактирования профия
-closeEditProfilePopupButton.addEventListener("click", () => {
-  closePopup(editProfilePopup);
+closingEditingProfilePopupButton.addEventListener("click", () => {
+  closePopup(editingProfilePopup);
 });
 
 // нажатие кнопки сохранить
-editProfileFormSubmitButton.addEventListener("click", (event) => {
+editingProfileFormSubmitButton.addEventListener("click", (event) => {
   event.preventDefault();
   profileUserName.textContent = formUserName.value;
   profileUserOccupation.textContent = formUserOccupation.value;
-  closePopup(editProfilePopup);
+  closePopup(editingProfilePopup);
 });
 
 ////////////////////////////////
@@ -83,6 +82,7 @@ editProfileFormSubmitButton.addEventListener("click", (event) => {
 // добавление на страницу начальных карточек
 
 function createNewCard(card) {
+  const elementsSection = document.querySelector('.elements');
   const elementTemplate = document
     .querySelector("#elementTemplate")
     .content.cloneNode(true);
@@ -92,7 +92,7 @@ function createNewCard(card) {
   elementUrl.setAttribute("src", card.url);
   elementAlt.setAttribute("alt", card.alt);
   elementHeading.textContent = card.heading;
-  elements.prepend(elementTemplate);
+  elementsSection.prepend(elementTemplate);
 }
 cards.forEach(createNewCard);
 
@@ -105,7 +105,7 @@ trashButtons.forEach((button) => {
 });
 
 // НАЖАТИЕ НА ИЗОБРАЖЕНИЕ
-const fullcreenButtons = document.querySelectorAll(".element__fullscreen");
+const increaseImageButtons = document.querySelectorAll(".element__fullscreen");
 // функция открытия полной версии изображения
 function openImage(event) {
   const image = imagePopup.querySelector(".popup__image");
@@ -119,14 +119,14 @@ function openImage(event) {
   image.nextElementSibling.textContent = imageData.caption;
   openPopup(imagePopup);
 }
-fullcreenButtons.forEach((button) => {
+increaseImageButtons.forEach((button) => {
   button.addEventListener("click", (event) => {
     openImage(event);
   });
 });
 // нажатие кнопки закрытия попапа изображения
-const closeImagePopupButton = document.querySelector("#closeImagePopupButton");
-closeImagePopupButton.addEventListener("click", () => closePopup(imagePopup));
+const closingImagePopupButton = document.querySelector("#closeImagePopupButton");
+closingImagePopupButton.addEventListener("click", () => closePopup(imagePopup));
 
 // ФУНКЦИОНАЛ ЛАЙКА
 const likes = document.querySelectorAll(".element__like");
@@ -137,27 +137,27 @@ likes.forEach((button) => {
 });
 
 // ДОБАВЛЕНИЕ НОВЫХ КАРТОЧЕК
-const addCardPopup = document.querySelector("#addCardPopup");
-const addCardButton = document.querySelector("#addCardButton");
-const closeAddCardPopupButton = document.querySelector(
+const additionCardPopup = document.querySelector("#addCardPopup");
+const additionCardButton = document.querySelector("#addCardButton");
+const closingAdditionCardPopupButton = document.querySelector(
   "#closeAddCardPopupButton"
 );
 
-addCardButton.addEventListener("click", () => {
-  openPopup(addCardPopup);
+additionCardButton.addEventListener("click", () => {
+  openPopup(additionCardPopup);
 });
 
-closeAddCardPopupButton.addEventListener("click", () => {
-  closePopup(addCardPopup);
+closingAdditionCardPopupButton.addEventListener("click", () => {
+  closePopup(additionCardPopup);
 });
 
-const addCardformSubmitButton = document.querySelector(
+const additionCardformSubmitButton = document.querySelector(
   "#addCardformSubmitButton"
 );
 let formCardName = document.querySelector("#formCardName");
 let formCardUrl = document.querySelector("#formCardUrl");
 
-addCardformSubmitButton.addEventListener("click", (event) => {
+additionCardformSubmitButton.addEventListener("click", (event) => {
   event.preventDefault();
   const newCard = {
     url: formCardUrl.value,
@@ -170,18 +170,18 @@ addCardformSubmitButton.addEventListener("click", (event) => {
   formCardUrl.value = "";
 
   // добавление функционала новой карточке
-  let newLike = document.querySelector(".element__like");
-  newLike.addEventListener("click", () => {
-    newLike.classList.toggle("element__like_active");
+  let like = document.querySelector(".element__like");
+  like.addEventListener("click", () => {
+    like.classList.toggle("element__like_active");
   });
 
-  let newTrashButton = document.querySelector(".element__trash");
-  newTrashButton.addEventListener("click", () => {
-    newTrashButton.closest(".element").remove();
+  let trashButton = document.querySelector(".element__trash");
+  trashButton.addEventListener("click", () => {
+    trashButton.closest(".element").remove();
   });
 
-  let newFullcreenButton = document.querySelector(".element__fullscreen");
-  newFullcreenButton.addEventListener("click", (event) => {
+  let increaseImageButton = document.querySelector(".element__fullscreen");
+  increaseImageButton.addEventListener("click", (event) => {
     openImage(event);
   });
 });
